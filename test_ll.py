6579f7869043d134dd95aef1b44dec8089cad74e
Espiobest <59823894+Espiobest@users.noreply.github.com>
2021-10-31 01:34:20 +0530

    fix spacing

--- test_ll.py
+++ test_ll.py
@@ -3,10 +3,13 @@ from LinkedList import DoublyLinkedList
 ll2 = DoublyLinkedList()
 ll2.add(2)
 print(ll2)
+
 ll2.append((4, 5, 6))
 print(ll2)
-print(ll2.__getitem__(1))
+print(ll2[1])
+
 ll2.__setitem__(0, [2, 14])
 print(ll2)
+
 ll2.__delitem__(1)
 print(ll2)
