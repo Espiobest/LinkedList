8796cf8096e2fd91a3ebf9670a9ecd055dbcc82b
Espiobest <59823894+Espiobest@users.noreply.github.com>
2021-10-31 01:39:13 +0530

    added more examples

--- test_ll.py
+++ test_ll.py
@@ -1,15 +1,21 @@
 from LinkedList import DoublyLinkedList
 
+ll = DoublyLinkedList()
+ll.add(2)
+print(ll)
+
+ll.append((4, 5, 6))
+print(ll)
+print(ll[1])
+
+ll[0] = [2, 14]  # ll.__setitem__(0, [2, 14])
+print(ll)
+
+del ll[1]  # ll.__delitem__(1)
+print(ll)
+
 ll2 = DoublyLinkedList()
-ll2.add(2)
-print(ll2)
+ll2.add("abc")
 
-ll2.append((4, 5, 6))
-print(ll2)
-print(ll2[1])
-
-ll2.__setitem__(0, [2, 14])
-print(ll2)
-
-ll2.__delitem__(1)
-print(ll2)
+print(ll + ll2)
+print(ll2 + ll)
